167c1c8eae1c370abe97b8879435adc7e4f51401
Mecami <826374247@qq.com>
2019-10-19 22:39:01 +0800

    导入了better-scroll框架

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,210 +1,148 @@
 <template>
-    <div id="home">
-        <!-- 导航栏 -->
-         <nav-bar class="home-nav">
-             <div slot="center" class="center">购物街</div>
-         </nav-bar>
-        <!-- 轮播图部分 -->
-        <home-swiper :banners="banners" />
-        <!-- 推荐部分 -->
-        <recommend :recommends="recommends"></recommend>
-        <!-- 本周流行 -->
-        <feature-view />
-        <!-- 选项栏部分 -->
-        <tab-control @getGoodsType="getType" class="tabbar-control" :titles="['流行','新款','精选']"></tab-control>
-        <!-- 商品列表 -->
-        <goods-list :goods-list="goods[goodsType].list" />> 
-        <ul>
-            <li>列表1</li>
-            <li>列表2</li>
-            <li>列表3</li>
-            <li>列表4</li>
-            <li>列表5</li>
-            <li>列表6</li>
-            <li>列表7</li>
-            <li>列表8</li>
-            <li>列表9</li>
-            <li>列表10</li>
-            <li>列表11</li>
-            <li>列表12</li>
-            <li>列表13</li>
-            <li>列表14</li>
-            <li>列表15</li>
-            <li>列表16</li>
-            <li>列表17</li>
-            <li>列表18</li>
-            <li>列表19</li>
-            <li>列表20</li>
-            <li>列表21</li>
-            <li>列表22</li>
-            <li>列表23</li>
-            <li>列表24</li>
-            <li>列表25</li>
-            <li>列表26</li>
-            <li>列表27</li>
-            <li>列表28</li>
-            <li>列表29</li>
-            <li>列表30</li>
-            <li>列表31</li>
-            <li>列表32</li>
-            <li>列表33</li>
-            <li>列表34</li>
-            <li>列表35</li>
-            <li>列表36</li>
-            <li>列表37</li>
-            <li>列表38</li>
-            <li>列表39</li>
-            <li>列表40</li>
-            <li>列表41</li>
-            <li>列表42</li>
-            <li>列表43</li>
-            <li>列表44</li>
-            <li>列表45</li>
-            <li>列表46</li>
-            <li>列表47</li>
-            <li>列表48</li>
-            <li>列表49</li>
-            <li>列表50</li>
-            <li>列表51</li>
-            <li>列表52</li>
-            <li>列表53</li>
-            <li>列表54</li>
-            <li>列表55</li>
-            <li>列表56</li>
-            <li>列表57</li>
-            <li>列表58</li>
-            <li>列表59</li>
-            <li>列表60</li>
-            <li>列表61</li>
-            <li>列表62</li>
-            <li>列表63</li>
-            <li>列表64</li>
-            <li>列表65</li>
-            <li>列表66</li>
-            <li>列表67</li>
-            <li>列表68</li>
-            <li>列表69</li>
-            <li>列表70</li>
-            <li>列表71</li>
-            <li>列表72</li>
-            <li>列表73</li>
-            <li>列表74</li>
-            <li>列表75</li>
-            <li>列表76</li>
-            <li>列表77</li>
-            <li>列表78</li>
-            <li>列表79</li>
-            <li>列表80</li>
-            <li>列表81</li>
-            <li>列表82</li>
-            <li>列表83</li>
-            <li>列表84</li>
-            <li>列表85</li>
-            <li>列表86</li>
-            <li>列表87</li>
-            <li>列表88</li>
-            <li>列表89</li>
-            <li>列表90</li>
-            <li>列表91</li>
-            <li>列表92</li>
-            <li>列表93</li>
-            <li>列表94</li>
-            <li>列表95</li>
-            <li>列表96</li>
-            <li>列表97</li>
-            <li>列表98</li>
-            <li>列表99</li>
-            <li>列表100</li>
-        </ul>
-    </div>
+  <div id="home">
+    <!-- 导航栏 -->
+    <nav-bar class="home-nav">
+      <div slot="center" class="center">购物街</div>
+    </nav-bar>
+
+    <scroll class="wrapper">
+      <!-- 轮播图部分 -->
+      <home-swiper :banners="banners" />
+      <!-- 推荐部分 -->
+      <recommend :recommends="recommends"></recommend>
+      <!-- 本周流行 -->
+      <feature-view />
+      <!-- 选项栏部分 -->
+      <tab-control @getGoodsType="getType" class="tabbar-control" :titles="['流行','新款','精选']"></tab-control>
+      <!-- 商品列表 -->
+      <goods-list :goods-list="showGoods" />
+    </scroll>
+
+
+  </div>
 </template>
 
 <script>
-//导入子组件
-import HomeSwiper from './childComps/HomeSwiper'
-import Recommend from './childComps/RecommendView'
-import FeatureView from './childComps/Feature'
-//导入公共组件
-import NavBar from 'components/common/navbar/NavBar'
-import TabControl from 'components/content/tabControl/TabControl'
-import GoodsList from 'components/content/goods/GoodsList'
-//导入用到的方法
-import {getHomeMultidata, getHomeGoods} from 'network/home'
+  //导入子组件
+  import HomeSwiper from './childComps/HomeSwiper'
+  import Recommend from './childComps/RecommendView'
+  import FeatureView from './childComps/Feature'
+  //导入公共组件
+  import NavBar from 'components/common/navbar/NavBar'
+  import TabControl from 'components/content/tabControl/TabControl'
+  import GoodsList from 'components/content/goods/GoodsList'
+  import Scroll from 'components/common/scroll/Scroll'
+  //导入用到的方法
+  import {
+    getHomeMultidata,
+    getHomeGoods
+  } from 'network/home'
 
-export default {
+  export default {
     name: 'Home',
-    data(){
-        return {
-            banners: [],
-            recommends: [],
-            goods: {
-                pop: {page: 0, list: []},
-                new: {page: 0, list: []},
-                sell: {page: 0, list: []},
-            },
-            goodsType: "pop",
-            goodsTypes: ['pop','new','sell']
-        }
+    data() {
+      return {
+        banners: [],
+        recommends: [],
+        goods: {
+          pop: {
+            page: 0,
+            list: []
+          },
+          new: {
+            page: 0,
+            list: []
+          },
+          sell: {
+            page: 0,
+            list: []
+          },
+        },
+        currentType: "pop",
+        goodsTypes: ['pop', 'new', 'sell']
+      }
     },
-    components :{
-        HomeSwiper,
-        Recommend,
-        FeatureView,
-        NavBar,
-        TabControl,
-        GoodsList
+    components: {
+      HomeSwiper,
+      Recommend,
+      FeatureView,
+      NavBar,
+      TabControl,
+      GoodsList,
+      Scroll
+    },
+    computed: {
+      showGoods() {
+        return this.goods[this.currentType].list
+      }
     },
     methods: {
-        getHomeMultidata() {
-            getHomeMultidata().then(res =>{
-            this.banners = res.data.banner.list
-            this.recommends = res.data.recommend.list
+      //事件监听相关方法
+      getType(index) {
+        this.currentType = this.goodsTypes[index];
+
+      },
+      //网络请求相关方法
+      getHomeMultidata() {
+        getHomeMultidata().then(res => {
+          this.banners = res.data.banner.list
+          this.recommends = res.data.recommend.list
         })
-        },
-        getHomeGoods(type){
-            const page = this.goods[type].page + 1
-            getHomeGoods(type, page).then(res => {
-                this.goods[type].list.push(...res.data.list)
-                this.goods[type].page ++
-            })
-        },
-        getType(index){
-            this.goodsType = this.goodsTypes[index];
-     
-        }
+      },
+      getHomeGoods(type) {
+        const page = this.goods[type].page + 1
+        getHomeGoods(type, page).then(res => {
+          this.goods[type].list.push(...res.data.list)
+          this.goods[type].page++
+        })
+      }
+
     },
-    
+
     created() {
-        //请求轮播图和推荐商品的数据
-        this.getHomeMultidata();
-        //请求商品列表
-        this.getHomeGoods('pop')
-        this.getHomeGoods('new')
-        this.getHomeGoods('sell')
+      //请求轮播图和推荐商品的数据
+      this.getHomeMultidata();
+      //请求商品列表
+      this.getHomeGoods('pop')
+      this.getHomeGoods('new')
+      this.getHomeGoods('sell')
     },
-    
-}
+
+  }
 </script>
 
-<style >
-    #home {
-        margin-top: 44px;
-    }
-    .home-nav {
-        background-color: rgb(248, 130, 159);
-        position: fixed;
-        top: 0;
-        left: 0;
-        right: 0;
-        z-index: 10;
-        /* box-shadow: 0 1px 1px rgba(10, 10, 10 , .3) */
-    }
-    .center {
-        color: #fff;
-    }
+<style>
+  #home {
+    /* margin-top: 44px; */
+    position: relative;
+    height: 100vh;
+  }
 
-    .tabbar-control {
-        position: sticky;
-        top: 44px;
-    }
+  .home-nav {
+    background-color: rgb(248, 130, 159);
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    z-index: 10;
+    /* box-shadow: 0 1px 1px rgba(10, 10, 10 , .3) */
+  }
+
+  .center {
+    color: #fff;
+  }
+
+  .tabbar-control {
+    position: sticky;
+    top: 44px;
+  }
+
+  .wrapper {
+      
+      position: absolute;
+      top: 44px;
+      bottom: 49px;
+      left: 0;
+      right: 0;
+  }
 </style>
