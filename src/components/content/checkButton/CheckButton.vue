aef2c6ba926d81909662391e23abc6e273ca70de
Mecami <826374247@qq.com>
2019-11-02 22:37:53 +0800

    实现CartListItem组件的内容布局

---
+++ src/components/content/checkButton/CheckButton.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="check-button" :class="{isCheck: check}">
+      <img src="~assets/img/cart/tick.svg" alt="">
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+        check: {
+            type: Boolean,
+            default: true
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .check-button {
+        width: 30px;
+        height: 30px;
+        border-radius: 50%;
+        border: 1px solid #ddd;
+    }
+    .isCheck {
+        background-color: red;
+        border-color: red;
+    }
+</style>
--- src/views/cart/childComps/CartListItem.vue
+++ src/views/cart/childComps/CartListItem.vue
@@ -1,10 +1,27 @@
 <template>
   <div class="cart-item">
-      {{product}}
+       <div class="check-button">
+           <check-button />
+       </div>
+        
+        <div class="item-img">
+            <img :src="product.image" alt="">
+        </div>
+
+        <div class="item-info">
+            <div class="item-title">{{product.title}}</div>
+            <div class="item-desc">{{product.desc}}</div>
+            <div class="item-bottom">
+                <div class="price">￥{{product.realPrice}}</div>
+                <div class="count">x1</div>
+            </div>
+        </div>
+        
   </div>
 </template>
 
 <script>
+import CheckButton from 'components/content/checkButton/CheckButton'
 export default {
     props: {
         product: {
@@ -13,10 +30,57 @@ export default {
                 return {}
             }
         }
+    },
+    components: {
+        CheckButton
     }
 }
 </script>
 
 <style scoped>
+    .cart-item {
+        /* width: 100%; */
+        border-bottom: 2px solid #ddd;
+        display: flex;
+        padding: 5px;
+    }
+    .check-button {
+        width: 30px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+     
+    .item-img{
+        margin: 0 10px;
+    }
+    .item-img img {
+         height: 150px;
+        width: 110px;
+        border-radius: 5px;
 
+    }
+    .item-info {
+        overflow: hidden;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+    }
+    .item-desc {
+        /* margin: 20px 0; */
+    }
+    .item-title, .item-desc {
+         /* 一行显示，溢出隐藏，设置省略号 */
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+    }
+    
+    .item-bottom {
+        display: flex;
+        justify-content: space-between;
+    }
+    .price {
+        color: red;
+    }
 </style>
